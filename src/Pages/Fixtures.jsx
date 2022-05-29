d3261d8f53e8992204a332fec36c759e72f8dac0
starkweb108 <ymomoh6@outlook.com>
2022-05-29 22:32:04 +0100

    added the search fixture feature

--- src/Pages/Fixtures.jsx
+++ src/Pages/Fixtures.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useGetFixturesQuery } from '../services/footballInfoApi';
 import logo from '../preloader.png';
 import moment from 'moment';
@@ -16,16 +16,31 @@ function Fixtures() {
   );
 
   const { data, isLoading } = useGetFixturesQuery(date);
-  function handleInput(e) {
-    e.preventDefault();
-    setDate(e.target.value);
-  }
+  const [searchField, setSearchField] = useState('');
+
   if (isLoading) {
     return (
       <div className="preloader--container">
         <img src={logo} alt="" />
       </div>
     );
+  }
+
+  const trying = searchField || data?.response;
+  console.log(trying);
+
+  function handleInput(e) {
+    e.preventDefault();
+    setDate(e.target.value);
+  }
+
+  function getFixturesBySearch(e) {
+    let values = data?.response.filter((country) => {
+      return country.league.country
+        .toLowerCase()
+        .includes(e.target.value.toLowerCase());
+    });
+    setSearchField(values);
   }
 
   return (
@@ -37,61 +52,78 @@ function Fixtures() {
         value={date}
         onChange={handleInput}
       />
+      <br />
+      <input
+        type="text"
+        name="search"
+        id="search"
+        placeholder="Search fixture by country name"
+        onChange={getFixturesBySearch}
+      />
+      {trying.length === 0 ? (
+        <h1>no fixture today for this country</h1>
+      ) : (
+        <article>
+          {trying.map((fixtures) => {
+            return (
+              <div key={fixtures.fixture.id}>
+                <div className="fixtures--header">
+                  <img
+                    src={
+                      fixtures.league.flag ||
+                      'https://logos-download.com/wp-content/uploads/2017/11/FIFA_logo_colored.png'
+                    }
+                    alt=""
+                  />
+                  <p>{fixtures.league.country}</p>
+                </div>
+                <div className="fixtures--league--logo">
+                  <img src={fixtures.league.logo} alt="" />
+                  <Link to={`/leagues/${fixtures.league.id}`}>
+                    <p>{fixtures.league.name}</p>
+                  </Link>
+                </div>
 
-      <article>
-        {data?.response.map((fixtures) => {
-          return (
-            <div key={fixtures.fixture.id}>
-              <div className="fixtures--header">
-                <img
-                  src={
-                    fixtures.league.flag ||
-                    'https://logos-download.com/wp-content/uploads/2017/11/FIFA_logo_colored.png'
-                  }
-                  alt=""
-                />
-                <p>{fixtures.league.country}</p>
-              </div>
-              <div className="fixtures--league--logo">
-                <img src={fixtures.league.logo} alt="" />
-                <Link to={`/leagues/${fixtures.league.id}`}>
-                  <p>{fixtures.league.name}</p>
+                <Link to={`${fixtures.fixture.id}`}>
+                  <div className="fixtures--playing--team--container">
+                    <div className="fixture--game--time">
+                      <small>
+                        {new Date(fixtures.fixture.date).toLocaleDateString()}
+                      </small>{' '}
+                      <small>
+                        {moment(fixtures.fixture.date).format('h:mm a')}
+                      </small>
+                      <small style={{ color: 'red' }}>
+                        {fixtures.fixture.status.elapsed}
+                      </small>
+                    </div>
+                    <section>
+                      <div className="fixtures--team--container">
+                        <span className="team">
+                          {' '}
+                          {fixtures.teams.home.name}{' '}
+                        </span>
+                        <span style={{ color: 'red' }}>
+                          {fixtures.goals.home}
+                        </span>
+                      </div>
+                      <div className="fixtures--team--container">
+                        <span className="team">
+                          {' '}
+                          {fixtures.teams.away.name}{' '}
+                        </span>
+                        <span style={{ color: 'red' }}>
+                          {fixtures.goals.away}
+                        </span>
+                      </div>
+                    </section>
+                  </div>
                 </Link>
               </div>
-
-              <Link to={`${fixtures.fixture.id}`}>
-                <div className="fixtures--playing--team--container">
-                  <div className="fixture--game--time">
-                    <small>
-                      {new Date(fixtures.fixture.date).toLocaleDateString()}
-                    </small>{' '}
-                    <small>
-                      {moment(fixtures.fixture.date).format('h:mm a')}
-                    </small>
-                    <small style={{ color: 'red' }}>
-                      {fixtures.fixture.status.elapsed}
-                    </small>
-                  </div>
-                  <section>
-                    <div className="fixtures--team--container">
-                      <span className="team"> {fixtures.teams.home.name} </span>
-                      <span style={{ color: 'red' }}>
-                        {fixtures.goals.home}
-                      </span>
-                    </div>
-                    <div className="fixtures--team--container">
-                      <span className="team"> {fixtures.teams.away.name} </span>
-                      <span style={{ color: 'red' }}>
-                        {fixtures.goals.away}
-                      </span>
-                    </div>
-                  </section>
-                </div>
-              </Link>
-            </div>
-          );
-        })}
-      </article>
+            );
+          })}
+        </article>
+      )}
     </section>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import { createRoot } from 'react-dom/client';
 import './scss/App.scss';
 import App from './App';
