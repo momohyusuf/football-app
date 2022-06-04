e1fcd770b9f5041e3a0f7c6188b78746ed8ec90a
starkweb108 <ymomoh6@outlook.com>
2022-06-04 07:45:02 +0100

    some issues fixed

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import { store } from './store';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
--- src/services/footballNewsApi.js
+++ src/services/footballNewsApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const requestHeaders = {
   'X-BingApis-SDK': 'true',
   'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com',
-  'X-RapidAPI-Key': 'c7e4f03fd0msh0675d167de2b9a4p1d8397jsn65da7e6f70d3',
+  'X-RapidAPI-Key': process.env.REACT_APP_SECRET_KEY,
 };
 const createRequest = (url) => ({
   url,
